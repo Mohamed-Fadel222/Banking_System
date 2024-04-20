f19d6cad7a079b183f75453c729f0176e28ae185
Mohamed-Fadel222 <116431641+Mohamed-Fadel222@users.noreply.github.com>
2024-04-20 18:19:25 +0200

    Making Deposit and Withdraw windows

---
+++ Deposit.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Deposit {
+    private JLabel Withdraw;
+    private JButton home;
+    private JButton confirmButton;
+    private JTextField textField1;
+}
---
+++ Withdraw.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Withdraw {
+    private JLabel Withdraw;
+    private JButton home;
+    private JButton confirmButton;
+    private JTextField textField1;
+}
